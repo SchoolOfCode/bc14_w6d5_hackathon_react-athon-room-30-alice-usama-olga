ccdb008fc186b27fecb6ca69f7fa10145f21c22c
usama-asghar1 <usama.asghar@hotmail.co.uk>
2023-06-13 11:58:31 +0100

    c

--- my-app/src/components/App/App.js
+++ my-app/src/components/App/App.js
@@ -1,24 +1,35 @@
 import './App.css';
 import RecipeText from '../RecipeText/index';
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   const [showRecipe, setShowRecipe] = useState(false);
+  const [theme, setTheme] = useState('light'); // track the current theme
 
   function handleClick() {
     setShowRecipe(true);
     console.log('Button clicked');
   }
 
+  function toggleTheme() {
+    setTheme(theme === 'light' ? 'dark' : 'light'); // toggle the theme
+  }
+
   return (
-    <div className="App">
+    <div className={`App ${theme}`}>
       <button onClick={handleClick}>Press me</button>
+      <button onClick={toggleTheme}>
+        <FontAwesomeIcon icon={theme === 'light' ? faMoon : faSun} />
+      </button>
       {showRecipe && <RecipeText setShowRecipe={setShowRecipe} />}
     </div>
   );
 }
 
 export default App;
+
 
 
 
